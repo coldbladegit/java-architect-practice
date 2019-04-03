c6c834b2629eb99db52d8d6ff40b46058d43e7e7
fu.luo@56qq.com <fu.luo@56qq.com>
2019-04-03 14:53:41 +0800

    init the architect-practice project

---
+++ src/main/java/com/cold/blade/architect/ArchitectPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.cold.blade.architect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cold/blade/architect/ArchitectPracticeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cold.blade.architect;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArchitectPracticeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
